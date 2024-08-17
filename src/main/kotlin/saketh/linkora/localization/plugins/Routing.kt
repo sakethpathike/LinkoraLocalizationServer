d06e2071b8cfa4f01e9da1d165e6a4bc0084d4a6
Saketh Pathike <sakethpathike@gmail.com>
2024-08-17 17:15:56 +0530

    Fix accessing language files

--- src/main/kotlin/saketh/linkora/localization/plugins/Routing.kt
+++ src/main/kotlin/saketh/linkora/localization/plugins/Routing.kt
@@ -1,29 +1,35 @@
 package saketh.linkora.localization.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import jdk.jfr.internal.JVM
 import saketh.linkora.localization.Route
 import java.io.File
 
 fun Application.configureRouting() {
+    val localizedStrings = LocalizedStrings()
     routing {
         get("/") {
             call.respondRedirect("https://github.com/sakethpathike/Linkora")
         }
 
         get(Route.ENGLISH.route) {
-            call.respondText(retrieveLocalizedStrings(Route.ENGLISH))
+            call.respondText(localizedStrings.retrieveLocalizedStrings(Route.ENGLISH))
         }
 
         get(Route.HINDI.route) {
-            call.respondText(retrieveLocalizedStrings(Route.HINDI))
+            call.respondText(localizedStrings.retrieveLocalizedStrings(Route.HINDI))
         }
     }
 }
 
 
-private fun retrieveLocalizedStrings(languageCode: Route): String {
-    val enFile = File("src/main/resources/raw/${languageCode.route.substringAfter("/")}.xml")
-    return enFile.readText()
+class LocalizedStrings {
+    fun retrieveLocalizedStrings(languageCode: Route): String {
+        val file =
+            this::class.java.getResourceAsStream("/raw/${languageCode.route.substringAfter("/")}.xml")
+        return file.use { it?.bufferedReader()?.readText().toString() }
+    }
 }
